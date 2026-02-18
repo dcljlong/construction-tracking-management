3a6ea330a1095b54fb39b31acb52e65e2fff3413
David <dcljlong@gmail.com>
2026-02-19 06:35:31 +1300

    chore: enable react-router v7 future flags

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+﻿
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
@@ -28,3 +28,4 @@ const App = () => (
 );
 
 export default App;
+
